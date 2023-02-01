13d35e50c2cfec68f2e613a7a143a6e2c254df48
新城旧梦 <39694377+itxcjm@users.noreply.github.com>
2023-02-01 14:26:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function generateProgressBar() {
 const readme = `\
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
-⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/itxcjm/itxcjm/workflows/Progress%20Bar%20CI/badge.svg)
 ### Blog：https://www.itxcjm.top
 ![GithubStats](https://github-readme-stats.vercel.app/api?username=itxcjm&show_icons=true&theme=light&layout=compact)
 ![Most Used Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=itxcjm&theme=light&layout=compact)\
